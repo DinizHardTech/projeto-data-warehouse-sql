0dfc0900a87a9a0b6a38ca4da8cc4efad5fe7c91
Pedro Diniz <148476392+DinizHardTech@users.noreply.github.com>
2026-03-24 11:12:04 -0300

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -29,175 +29,81 @@ Boas práticas:
 
 
 
-CREATE OR ALTER PROCEDURE bronze.load_bronze 
-AS
-BEGIN
-    DECLARE 
-        @start_time DATETIME,
-        @end_time DATETIME,
-        @batch_start_time DATETIME,
-        @batch_end_time DATETIME;
+IF OBJECT_ID ('bronze.crm_cust_info' , 'U') IS NOT NULL
+DROP TABLE bronze.crm_cust_info;
 
-    BEGIN TRY
+CREATE TABLE bronze.crm_cust_info (
 
-        SET @batch_start_time = GETDATE();
+	cst_id INT,
+	cst_key NVARCHAR(50),
+	cst_firstname NVARCHAR(50),
+	cst_lastname NVARCHAR(50),
+	cst_material_status NVARCHAR(50),
+	cst_gndr NVARCHAR(50),
+	cst_create_date DATE
 
-        PRINT '===========================';
-        PRINT 'Carregamento Camada Bronze';
-        PRINT '===========================';
+);
 
-        -- =====================================
-        -- CRM - CUST INFO
-        -- =====================================
-        SET @start_time = GETDATE();
 
-        PRINT '>> TRUNCATE TABLE: bronze.crm_cust_info';
-        TRUNCATE TABLE bronze.crm_cust_info;
+IF OBJECT_ID ('bronze.prd.cust_info' , 'U') IS NOT NULL
+DROP TABLE bronze.crm_prd_info;
 
-        PRINT '>> INSERTING DATA INTO: bronze.crm_cust_info';
-        BULK INSERT bronze.crm_cust_info
-        FROM 'C:\Users\bocca\OneDrive\Área de Trabalho\Projetos Portifólio\Datawherehouse Projeto\fonte_CRM\cust_info.csv'
-        WITH (
-            FIRSTROW = 2,
-            FIELDTERMINATOR = ',',
-            TABLOCK
-        );
 
-        SET @end_time = GETDATE();
-        PRINT '>> Duracao: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' segundos';
+CREATE TABLE bronze.crm_prd_info (
+    prd_id INT,
+    prd_key NVARCHAR(50),
+    prd_nm NVARCHAR(50),
+    prd_cost INT,
+    prd_line NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt DATETIME
+);
 
-        -- =====================================
-        -- CRM - PRD INFO
-        -- =====================================
-        SET @start_time = GETDATE();
 
-        PRINT '--------------------------';
-        PRINT 'Carregamento Tabelas CRM';
-        PRINT '--------------------------';
+IF OBJECT_ID ('bronze.crm_sales_details' , 'U') IS NOT NULL
+DROP TABLE bronze.crm_sales_details;
 
-        PRINT '>> TRUNCATE TABLE: bronze.crm_prd_info';
-        TRUNCATE TABLE bronze.crm_prd_info;
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num NVARCHAR(50),
+    sls_prd_key NVARCHAR(50),
+    sls_cust_id INT,
+    sls_order_dt INT,
+    sls_ship_dt INT,
+    sls_due_dt INT,
+    sls_sales INT,
+    sls_quantity INT,
+    sls_price INT
+);
 
-        PRINT '>> INSERTING DATA INTO: bronze.crm_prd_info';
-        BULK INSERT bronze.crm_prd_info
-        FROM 'C:\Users\bocca\OneDrive\Área de Trabalho\Projetos Portifólio\Datawherehouse Projeto\fonte_CRM\prd_info.csv'
-        WITH (
-            FIRSTROW = 2,
-            FIELDTERMINATOR = ',',
-            TABLOCK
-        );
 
-        SET @end_time = GETDATE();
-        PRINT '>> Duracao: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' segundos';
+IF OBJECT_ID ('bronze.erp_loc_a101' , 'U') IS NOT NULL
+DROP TABLE bronze.erp_loc_a101;
 
-        -- =====================================
-        -- CRM - SALES DETAILS
-        -- =====================================
-        SET @start_time = GETDATE();
 
-        PRINT '>> TRUNCATE TABLE: bronze.crm_sales_details';
-        TRUNCATE TABLE bronze.crm_sales_details;
+CREATE TABLE bronze.erp_loc_a101 (
+    cid NVARCHAR(50),
+    cntry NVARCHAR(50)
+);
 
-        PRINT '>> INSERTING DATA INTO: bronze.crm_sales_details';
-        BULK INSERT bronze.crm_sales_details
-        FROM 'C:\Users\bocca\OneDrive\Área de Trabalho\Projetos Portifólio\Datawherehouse Projeto\fonte_CRM\sales_details.csv'
-        WITH (
-            FIRSTROW = 2,
-            FIELDTERMINATOR = ',',
-            TABLOCK
-        );
 
-        SET @end_time = GETDATE();
-        PRINT '>> Duracao: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' segundos';
+IF OBJECT_ID ('bronze.erp_cust_az12' , 'U') IS NOT NULL
+DROP TABLE bronze.erp_cust_az12;
 
-        PRINT '--------------------------';
 
-        -- =====================================
-        -- ERP - CUST
-        -- =====================================
-        SET @start_time = GETDATE();
+CREATE TABLE bronze.erp_cust_az12 (
+    cid NVARCHAR(50),
+    bdate DATE,
+    gen NVARCHAR(50)
+);
 
-        PRINT 'Carregamento Tabelas ERP';
-        PRINT '--------------------------';
 
-        PRINT '>> TRUNCATE TABLE: bronze.erp_cust_az12';
-        TRUNCATE TABLE bronze.erp_cust_az12;
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2' , 'U') IS NOT NULL
+DROP TABLE bronze.erp_px_cat_g1v2;
 
-        PRINT '>> INSERTING DATA INTO: bronze.erp_cust_az12';
-        BULK INSERT bronze.erp_cust_az12
-        FROM 'C:\Users\bocca\OneDrive\Área de Trabalho\Projetos Portifólio\Datawherehouse Projeto\fonte_ERP\CUST_AZ12.csv'
-        WITH (
-            FIRSTROW = 2,
-            FIELDTERMINATOR = ',',
-            TABLOCK
-        );
 
-        SET @end_time = GETDATE();
-        PRINT '>> Duracao: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' segundos';
-
-        -- =====================================
-        -- ERP - LOC
-        -- =====================================
-        SET @start_time = GETDATE();
-
-        PRINT '>> TRUNCATE TABLE: bronze.erp_loc_a101';
-        TRUNCATE TABLE bronze.erp_loc_a101;
-
-        PRINT '>> INSERTING DATA INTO: bronze.erp_loc_a101';
-        BULK INSERT bronze.erp_loc_a101
-        FROM 'C:\Users\bocca\OneDrive\Área de Trabalho\Projetos Portifólio\Datawherehouse Projeto\fonte_ERP\LOC_A101.csv'
-        WITH (
-            FIRSTROW = 2,
-            FIELDTERMINATOR = ',',
-            TABLOCK
-        );
-
-        SET @end_time = GETDATE();
-        PRINT '>> Duracao: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' segundos';
-
-        -- =====================================
-        -- ERP - CATEGORY
-        -- =====================================
-        SET @start_time = GETDATE();
-
-        PRINT '>> TRUNCATE TABLE: bronze.erp_px_cat_g1v2';
-        TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-
-        PRINT '>> INSERTING DATA INTO: bronze.erp_px_cat_g1v2';
-        BULK INSERT bronze.erp_px_cat_g1v2
-        FROM 'C:\Users\bocca\OneDrive\Área de Trabalho\Projetos Portifólio\Datawherehouse Projeto\fonte_ERP\PX_CAT_G1V2.csv'
-        WITH (
-            FIRSTROW = 2,
-            FIELDTERMINATOR = ',',
-            TABLOCK
-        );
-
-        SET @end_time = GETDATE();
-        PRINT '>> Duracao: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' segundos';
-
-        -- =====================================
-        -- FINAL
-        -- =====================================
-        SET @batch_end_time = GETDATE();
-
-        PRINT '======================================';
-        PRINT 'Carregamento Camada Bronze Completa';
-        PRINT '>> Duracao Total: ' 
-            + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) 
-            + ' segundos';
-        PRINT '======================================';
-
-    END TRY
-    BEGIN CATCH
-        PRINT '===========================';
-        PRINT 'Falha ao carregar camada bronze';
-        PRINT 'Erro: ' + ERROR_MESSAGE();
-        PRINT 'Numero: ' + CAST(ERROR_NUMBER() AS NVARCHAR);
-        PRINT 'Estado: ' + CAST(ERROR_STATE() AS NVARCHAR);
-        PRINT '===========================';
-    END CATCH
-END
-GO
-
--- EXECUÇÃO
-EXEC bronze.load_bronze;
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id NVARCHAR(50),
+    cat NVARCHAR(50),
+    subcat NVARCHAR(50),
+    maintenance NVARCHAR(50)
+);
